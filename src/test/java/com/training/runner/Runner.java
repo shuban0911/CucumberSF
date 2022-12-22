b39a82a165ca3cf6c9731713bc7762781c4ccc81
shuban0911 <uma.sankari09@gmail.com>
2022-12-22 10:49:15 -0500

    Changed runner

--- src/test/java/com/training/runner/Runner.java
+++ src/test/java/com/training/runner/Runner.java
@@ -9,9 +9,9 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(plugin= {"pretty","html:target/cucumber-reports/cucumber.html",
 		                            "json:target/cucumber-reports/cucumber.json"},
                  features="./src/test/resources/features",
-                 glue= {"com.training.steps"},
+                 glue= {"com.training.steps"}
                 //dryRun=true
-                 tags="@new"
+                 //tags="@new"
                 
                					
 				 )
